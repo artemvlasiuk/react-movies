a8eab63f9226db6ffbf99181ff8e6b71f7068fe1
Artem Vlasiuk <artemvlasiuk93@gmail.com>
2022-03-29 16:02:27 +0300

    initial

---
+++ src/layout/Main.jsx
@@ -0,0 +1,27 @@
+import {Component} from 'react';
+import Movies from '../components/Movies';
+
+class Main extends Component {
+    state = {
+        movies: [],
+    }
+
+    componentDidMount() {
+        fetch('http://www.omdbapi.com/?apikey=2e916089&s=matrix')
+            .then(response => response.json())
+            .then(data => this.setState({movies: data.Search}))
+    }
+
+    render() {
+
+        const {movies} = this.state;
+
+        return <main className="container content">
+                    {
+                        movies.length ? (<Movies movies={this.state.movies}/>) : <h3>Loading...</h3>
+                    }
+                </main>
+    }
+}
+
+export default Main;
